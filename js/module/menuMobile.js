80a9b9af25b88df90246ab309f94308d51968ca7
RenanFumis <renan-oliveira19@hotmail.com>
2024-01-19 18:09:03 -0300

    menu mobile refatorado

--- js/module/menuMobile.js
+++ js/module/menuMobile.js
@@ -1,29 +1,40 @@
 import outsideClick from './clicandoForaMenuDropdown.js'; //Quando importar algo que está na mesma pasta é só começar com ./
-
-
-export default function menuMobileIniciar(){
-
-  const menuBotao = document.querySelector('[data-menu="button"]')
-  const menuLista = document.querySelector('[data-menu="lista"]')
-  const eventos = ['click', 'touchstart']
-
-  if(menuBotao){
-  
-      function abrirMenu(){
-        menuLista.classList.add('ativo');
-        menuBotao.classList.add('ativo');
-        outsideClick(menuLista, eventos, () =>{
-          menuLista.classList.remove('ativo');
-          menuBotao.classList.remove('ativo');
-          
-        })
-      }
-
-      eventos.forEach((e)=>{
-        menuBotao.addEventListener(e, abrirMenu, {passive: true})
-      })
-
-    menuBotao.addEventListener('click',abrirMenu)
+export default class MenuMobile{
+  constructor (menuBotao, menuLista, eventos){
+    this.menuBotao = document.querySelector(menuBotao)
+    this.menuLista = document.querySelector(menuLista)
+    this.classeAtiva = 'ativo'
+    //Define click e touchstart como argumento padrão de evento caso não seja definido
+    if(eventos === undefined){
+      this.eventos = ['click','touchstart']
+    } else {
+      this.eventos = eventos
+    }
+    
+    this.abrirMenu = this.abrirMenu.bind(this)
   }
+
+  abrirMenu(){
+    this.menuLista.classList.add(this.classeAtiva);
+    this.menuBotao.classList.add(this.classeAtiva);
+    outsideClick(this.menuLista, this.eventos, () =>{
+      this.menuLista.classList.remove(this.classeAtiva);
+      this.menuBotao.classList.remove(this.classeAtiva);
+      
+    })
+  }
+
+  addMenuMobileEvento(){
+    this.eventos.forEach((e)=>{
+      this.menuBotao.addEventListener(e, this.abrirMenu, {passive: true})
+    })
+  }
+  init(){
+    if(this.menuBotao && this.menuLista){
+      this.addMenuMobileEvento()
+    }
+    return this
+  }
+  
 }
 
